894110e39c517f76e5a5b5892e9759db8837804f
Zaed Abdullah <z4ed.thalib123@gmail.com>
2023-05-21 15:19:51 +0700

    mysql can now intregate with express

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+var bodyParser = require('body-parser');
+
+dotenv.config();
+// MySQL DB Connection 
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB
+})
+
+connection.connect()
+
+connection.query('SELECT 3 + 5 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
+
+//BodyParsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Routes
+// app.use('/', require('./routes/user'));
+const PORT = process.env.PORT || 4111;
+app.listen(PORT, console.log('Server has started at: http://localhost:' + PORT));
+
