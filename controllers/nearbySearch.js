e57c79d82726840da1ee4f19219f4cada65fd064
Zaed Abdullah <z4ed.thalib123@gmail.com>
2023-06-15 14:24:55 +0700

    update nearby search feature

--- controllers/nearbySearch.js
+++ controllers/nearbySearch.js
@@ -2,7 +2,9 @@ const fetch = require('node-fetch');
 
 const nearbySearch = async (req, res) => {
     try{
-        const location = '-7.985798637921982, 112.6276388035519';
+        const lat = req.query.latitude
+        const lng = req.query.longitude
+        const location = `${lat}, ${lng}`;
         const radius = '50000';
         const keyword = 'sanggar seni';
         const key = process.env.MAPS_KEY;
@@ -11,12 +13,12 @@ const nearbySearch = async (req, res) => {
             method: 'GET',
         });
         const data = await response.json();
-        return res.status(200).json({data: data});
+        return res.status(200).json({data: data.results});
     } catch(err){
         return res.status(500).send({ "error": `${err}` });
     }
 };
 
 module.exports = {
-    nearbySearch
+    nearbySearch,
 };
